a322ca285fa1d08d59efa8cbe74ee94b0406d74c
Alex Haurin <alex.haurin@gmail.com>
2020-10-08 15:04:45 -0400

    Update vectors.h

--- vectors.h
+++ vectors.h
@@ -7,37 +7,39 @@ void print(T message) {
 	std::cout << message << std::endl;
 }
 
-template<typename T>
-void addVectorItem(std::vector<T>& vector, T item) {
-	vector.push_back(item);
-}
-
-//Deletes item from vector using value
-template<typename T>
-void deleteVectorItem(std::vector<T>& vector, T item) {
-	for (unsigned int i = 0; i < vector.size(); i++) {
-		if (vector[i] == item) {
-			vector.erase(0);
-			break;
-		}
+namespace vectors {
+	template<typename T>
+	void addItem(std::vector<T>& vector, T item) {
+		vector.push_back(item);
 	}
-}
 
-//Finds item in vector using value
-template<typename T>
-bool findVectorItem(std::vector<T>& vector, T item) {
-	for (unsigned int i = 0; i < vector.size(); i++) {
-		if (vector[i] == item) {
-			return true;
+	//Deletes item from vector using value
+	template<typename T>
+	void deleteItem(std::vector<T>& vector, T item) {
+		for (unsigned int i = 0; i < vector.size(); i++) {
+			if (vector[i] == item) {
+				vector.erase(0);
+				break;
+			}
 		}
 	}
 
-	return false;
-}
+	//Finds item in vector using value
+	template<typename T>
+	bool findItem(std::vector<T>& vector, T item) {
+		for (unsigned int i = 0; i < vector.size(); i++) {
+			if (vector[i] == item) {
+				return true;
+			}
+		}
 
-template<typename T>
-void printVector(std::vector<T>& vector) {
-	for (unsigned int i = 0; i < vector.size(); i++) {
-		std::cout << vector[i] << ", ";
+		return false;
+	}
+
+	template<typename T>
+	void print(std::vector<T>& vector) {
+		for (unsigned int i = 0; i < vector.size(); i++) {
+			std::cout << vector[i] << ", ";
+		}
 	}
 }
