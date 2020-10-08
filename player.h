7d75c91684ae3ba2ad0df6b9f006e3a8348b6e32
Alex Haurin <alex.haurin@gmail.com>
2020-10-08 15:04:19 -0400

    Update player.h

--- player.h
+++ player.h
@@ -1,16 +1,20 @@
 #pragma once
 #include <string>
 #include <map>
+#include <iostream>
+#include <vector>
 
 class Player {
 public:
 	void getAttribute(const char* att);
+	bool challenge(const char* task, unsigned int dif);
+	void caught(const char* message);
 
 public:
 	std::vector<std::string> items;
-
 	unsigned int totalPoints = 10;
-	unsigned int difficulty = 0;
+	unsigned int level = 0;
+
 	std::map<const char*, unsigned int> attributes = {
 		{ "stealth", 0 },
 		{ "luck", 0 },
