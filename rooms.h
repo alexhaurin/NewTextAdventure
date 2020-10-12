f550de5de0447fada57bafde022800976c07e96b
Alex Haurin <alex.haurin@gmail.com>
2020-10-12 14:16:03 -0400

    Update rooms.h

--- rooms.h
+++ rooms.h
@@ -1,1 +1,20 @@
 #pragma once
+#include <vector>
+#include <string>
+#include "vectors.h"
+#include "player.h"
+
+class Room {
+public:
+	Room(std::vector<std::string> com, std::vector<std::string> f, std::vector<std::string> choices, std::vector<std::string> places, std::string item, const char* in);
+	void start(Player& player);
+public:
+	std::vector<std::string> completed;
+	std::vector<std::string> failed;
+	std::vector<std::string> choices;
+	std::vector<std::string> places;
+	std::string item;
+	const char* intro;
+};
+
+void travel(Player& player, std::vector<std::string> choices);
